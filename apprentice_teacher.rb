46369f3e9632733649d43c002452ca224191cba4
Paula Lee <pdlee877@gmail.com>
2016-09-01 09:52:51 -0700

    add teacher/student class and a teacher specific module

--- apprentice_teacher.rb
+++ apprentice_teacher.rb
@@ -1,21 +1,13 @@
-class ApprenticeTeacher
-  attr_reader :age, :salary, :phase, :target_raise
-  attr_accessor :name
+require_relative 'teacher_class'
+require_relative 'teacher_module'
 
+class ApprenticeTeacher < TeacherStudent
+  include Teachers
+  
   def initialize(options={})
-    @age = options.fetch(:age, 0)
-    @name = options.fetch(:name, "")
+    super
     @target_raise = 800
     @phase = 3
-  end
-
-  def offer_high_five
-    "High five!"
-  end
-
-  def set_phase(num)
-    @phase = num
-    "Cool, I've always wanted to teach phase #{num}!"
   end
 
   def teach_stuff
@@ -24,15 +16,6 @@ class ApprenticeTeacher
     response += "*drops crazy knowledge bomb* "
     response += "... You're welcome."
     response
-  end
-
-  def salary=(new_salary)
-    puts "This better be good!"
-    @salary = new_salary
-  end
-
-  def receive_raise(raise)
-    @salary += raise
   end
 
   def set_performance_rating(rating)
