06054bde1abcc066e8c88eef2249c9474bfb543f
Sam Parker <email": null>
2016-09-01 10:15:12 -0700

    refactor with person superclass

--- teacher.rb
+++ teacher.rb
@@ -1,16 +1,13 @@
-class Teacher
+require_relative 'person'
+
+class Teacher < Person
   attr_reader :age, :salary, :phase, :performance_rating, :target_raise
   attr_accessor :name
 
   def initialize(options={})
+    super
     @phase = 3
-    @age = options.fetch(:age, 0)
-    @name = options.fetch(:name, "")
     @target_raise = 1000
-  end
-
-  def offer_high_five
-    "High five!"
   end
 
   def set_phase(num)
