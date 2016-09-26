864992b594b6ec2c923e192b95463d00847c84d8
Sarah Spikes <sdspikes@cs.stanford.edu>
2016-09-26 14:48:33 -0700

    add new method to senior teacher and make set_performance_rating the same across both

--- senior_teacher.rb
+++ senior_teacher.rb
@@ -38,12 +38,16 @@ class SeniorTeacher
   def set_performance_rating(rating)
     response = ""
     if rating > 90
-      receive_raise(@target_raise)
       response = "Yay, I'm a great employee!"
+      receive_raise(@target_raise)
     else
       response += "Oh, well -- thanks to this actionable, specific, and kind "
       response += "feedback, I'll do better next time."
     end
     response
+  end
+
+  def lead_training_session
+    puts "Hey newbie!  Here are some common pitfalls.  Don't fall in them!"
   end
 end
