8f98bb278673c4d9d5209ccc04affba484bc8b4e
Robert Turner Jr <georob23@gmail.com>
2016-10-13 09:48:50 -0700

    Create Teacher super class

--- apprentice_teacher.rb
+++ apprentice_teacher.rb
@@ -1,21 +1,12 @@
-class ApprenticeTeacher
+require_relative 'teacher'
+
+class ApprenticeTeacher < Teacher
   attr_reader :age, :salary, :phase, :target_raise
   attr_accessor :name
 
   def initialize(options={})
-    @age = options.fetch(:age, 0)
-    @name = options.fetch(:name, "")
+    super
     @target_raise = 800
-    @phase = 3
-  end
-
-  def offer_high_five
-    "High five!"
-  end
-
-  def set_phase(num)
-    @phase = num
-    "Cool, I've always wanted to teach phase #{num}!"
   end
 
   def teach_stuff
@@ -24,15 +15,6 @@ class ApprenticeTeacher
     response += "*drops crazy knowledge bomb* "
     response += "... You're welcome."
     response
-  end
-
-  def salary=(new_salary)
-    puts "This better be good!"
-    @salary = new_salary
-  end
-
-  def receive_raise(raise)
-    @salary += raise
   end
 
   def set_performance_rating(rating)
--- teacher.rb
+++ teacher.rb
@@ -1,10 +1,8 @@
-require_relative 'teacher'
-
-class SeniorTeacher < Teacher
-  attr_reader :age, :salary, :phase, :performance_rating, :target_raise
+class Teacher
+  attr_reader :age, :salary, :phase, :target_raise
   attr_accessor :name
 
-  def initialize(options={})
+def initialize(options={})
     @phase = 3
     @age = options.fetch(:age, 0)
     @name = options.fetch(:name, "")
@@ -18,19 +16,6 @@ class SeniorTeacher < Teacher
   def set_phase(num)
     @phase = num
     "Cool, I've always wanted to teach phase #{num}!"
-  end
-
-  def teach_stuff
-    response = ""
-    response += "Listen, class, this is how everything works, fo SHO! "
-    response += "*drops flat-out insane knowledge bomb* "
-    response += "... You're welcome. *saunters away*"
-    response
-  end
-
-  def salary=(new_salary)
-    puts "This better be good!"
-    @salary = new_salary
   end
 
   def receive_raise(raise)
@@ -47,9 +32,5 @@ class SeniorTeacher < Teacher
       response += "feedback, I'll do better next time."
     end
     response
-  end
-
-  def lead_training_session
-    puts "Hey newbie!  Here are some common pitfalls.  Don't fall in them!"
   end
 end
